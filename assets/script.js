8b5fffcf880899f5cfc96040ad0b59ab47addef0
Jack Huddlestone <jack.huddlestone@littlefish.co.uk>
2023-12-05 20:25:29 +0000

    get azure function visitor counter working and returning correct value

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('https://func-cloud-resume-counter.azurewebsites.net/api/visitor-counter')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('HTTP status ' + response.status);
+        }
+        return response.text();
+    })
+    .then(data => {
+        console.log('Visitor count received:', data); // Log the data for debugging
+        document.getElementById('counter').innerText = data;
+    })
+    .catch(error => {
+        console.error('Fetch operation error:', error);
+        document.getElementById('counter').innerText = 'Error loading count: ' + error.message;
+    });
+});
