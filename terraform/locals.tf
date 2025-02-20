c6b09e50d7c8a9f535f62ccee1db3ec57a710602
juddlestone <jack@itsjack.cloud>
2025-02-20 21:29:28 +0000

    add first container app test

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,16 +1,26 @@
 locals {
+  application_name                = "cloudresume"
   custom_domain                   = var.custom_domain
   custom_domain_resource_friendly = replace(local.custom_domain, ".", "-")
   environment                     = var.environment
   location                        = var.location
 
+  user_assigned_identity_name    = module.naming.user_assigned_identity.name
   container_app_environment_name = module.naming.container_app_environment.name
   resource_group_name            = module.naming.resource_group.name
   log_analytics_workspace_name   = module.naming.log_analytics_workspace.name
 
-  # budget_name       = "budget-${module.naming.resource_group.name}"
-  # budget_start_date = formatdate("YYYY-MM-01'T'hh:mm:ssZ", time_static.this.rfc3339)
-  # budget_end_date   = timeadd(time_static.this.rfc3339, "26280h")
+  container_apps = {
+    "cloudresume" = {
+      name  = "frontend"
+      image = "mcr.microsoft.com/azuredocs/aks-helloworld:v1"
+      port  = 80
+    }
+  }
+
+  budget_name       = "budget-${module.naming.resource_group.name}"
+  budget_start_date = formatdate("YYYY-MM-01'T'hh:mm:ssZ", time_static.this.rfc3339)
+  budget_end_date   = timeadd(time_static.this.rfc3339, "26280h")
 
   tags = {
     "Environment"  = upper(local.environment)
--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {
 
 module "naming" {
   source = "Azure/naming/azurerm"
-  suffix = ["cloudresume", local.environment]
+  suffix = [local.application_name, local.environment]
 }
 
 
@@ -16,23 +16,23 @@ resource "azurerm_resource_group" "this" {
   tags = local.tags
 }
 
-# resource "azurerm_consumption_budget_resource_group" "this" {
-#   name              = local.budget_name
-#   resource_group_id = azurerm_resource_group.this.id
-#   amount            = 5
+resource "azurerm_consumption_budget_resource_group" "this" {
+  name              = local.budget_name
+  resource_group_id = azurerm_resource_group.this.id
+  amount            = 5
 
-#   time_period {
-#     start_date = local.budget_start_date
-#     end_date   = local.budget_end_date
-#   }
+  time_period {
+    start_date = local.budget_start_date
+    end_date   = local.budget_end_date
+  }
 
-#   notification {
-#     operator       = "GreaterThan"
-#     threshold      = 75
-#     threshold_type = "Actual"
-#     contact_roles  = ["Owner"]
-#   }
-# }
+  notification {
+    operator       = "GreaterThan"
+    threshold      = 75
+    threshold_type = "Actual"
+    contact_roles  = ["Owner"]
+  }
+}
 
 resource "azurerm_container_app_environment" "this" {
   name                       = local.container_app_environment_name
@@ -42,7 +42,6 @@ resource "azurerm_container_app_environment" "this" {
   logs_destination           = "log-analytics"
 
   tags = local.tags
-
 }
 
 resource "azurerm_log_analytics_workspace" "this" {
@@ -51,4 +50,35 @@ resource "azurerm_log_analytics_workspace" "this" {
   location            = azurerm_resource_group.this.location
   sku                 = "PerGB2018"
   retention_in_days   = 30
+}
+
+resource "azurerm_user_assigned_identity" "this" {
+  name                = local.user_assigned_identity_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_container_app" "this" {
+  for_each                     = local.container_apps
+  name                         = "ca-${each.value.name}-${local.application_name}-${local.environment}"
+  resource_group_name          = azurerm_resource_group.this.name
+  container_app_environment_id = azurerm_container_app_environment.this.id
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "ca-${each.value.name}-${local.application_name}-${local.environment}"
+      image  = each.value.image
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+
+  ingress {
+    target_port = each.value.port
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
 }
