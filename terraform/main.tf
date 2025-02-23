fa2a8f55f4f54dc89555eca71e1259dbceffecae
juddlestone <jack@itsjack.cloud>
2025-02-23 09:27:30 +0000

    Bring up infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,110 +8,110 @@
 
 # resource "time_static" "this" {}
 
-# resource "azurerm_resource_group" "this" {
-#   name     = local.resource_group_name
-#   location = local.location
+resource "azurerm_resource_group" "this" {
+  name     = local.resource_group_name
+  location = local.location
 
-#   tags = local.tags
-# }
+  tags = local.tags
+}
 
-# # resource "azurerm_consumption_budget_resource_group" "this" {
-# #   name              = local.budget_name
-# #   resource_group_id = azurerm_resource_group.this.id
-# #   amount            = 5
+# resource "azurerm_consumption_budget_resource_group" "this" {
+#   name              = local.budget_name
+#   resource_group_id = azurerm_resource_group.this.id
+#   amount            = 5
 
-# #   time_period {
-# #     start_date = local.budget_start_date
-# #     end_date   = local.budget_end_date
-# #   }
-
-# #   notification {
-# #     operator       = "GreaterThan"
-# #     threshold      = 75
-# #     threshold_type = "Actual"
-# #     contact_roles  = ["Owner"]
-# #   }
-# # }
-
-# resource "azurerm_log_analytics_workspace" "law" {
-#   name                = local.log_analytics_workspace_name
-#   location            = azurerm_resource_group.this.location
-#   resource_group_name = azurerm_resource_group.this.name
-#   sku                 = "PerGB2018"
-#   retention_in_days   = 30
-
-#   tags = local.tags
-# }
-
-# module "cae" {
-#   source  = "Azure/avm-res-app-managedenvironment/azurerm"
-#   version = "0.2.1"
-
-#   name                = local.container_app_environment_name
-#   resource_group_name = azurerm_resource_group.this.name
-#   location            = azurerm_resource_group.this.location
-
-#   log_analytics_workspace_customer_id        = azurerm_log_analytics_workspace.law.id
-#   log_analytics_workspace_primary_shared_key = azurerm_log_analytics_workspace.law.primary_shared_key
-
-#   tags = local.tags
-# }
-
-# # Container App
-# resource "azurerm_container_app" "this" {
-#   for_each                     = local.container_apps
-#   name                         = "ca-${each.key}-${local.application_name}-${local.environment}"
-#   resource_group_name          = azurerm_resource_group.this.name
-#   container_app_environment_id = module.cae.id
-#   revision_mode                = "Single"
-
-#   template {
-#     container {
-#       name   = "ca-${each.key}-${local.application_name}-${local.environment}"
-#       image  = each.value.image
-#       cpu    = 0.25
-#       memory = "0.5Gi"
-#     }
+#   time_period {
+#     start_date = local.budget_start_date
+#     end_date   = local.budget_end_date
 #   }
 
-#   ingress {
-#     target_port      = each.value.port
-#     external_enabled = each.value.external_enabled
-
-#     traffic_weight {
-#       percentage      = 100
-#       latest_revision = true
-#     }
-#   }
-
-#   registry {
-#     server   = "acrmanacr.azurecr.io"
-#     identity = azurerm_user_assigned_identity.this[each.key].id
-#   }
-
-#   identity {
-#     type = "UserAssigned"
-#     identity_ids = [
-#       azurerm_user_assigned_identity.this[each.key].id
-#     ]
+#   notification {
+#     operator       = "GreaterThan"
+#     threshold      = 75
+#     threshold_type = "Actual"
+#     contact_roles  = ["Owner"]
 #   }
 # }
 
-# # Container App - User Assigned Identity
-# resource "azurerm_user_assigned_identity" "this" {
-#   for_each            = local.container_apps
-#   name                = "uai-${each.key}-${local.application_name}-${local.environment}"
-#   location            = azurerm_resource_group.this.location
-#   resource_group_name = azurerm_resource_group.this.name
-# }
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = local.log_analytics_workspace_name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 
-# # Container App - ACR Pull
-# resource "azurerm_role_assignment" "acrpull" {
-#   for_each             = local.container_apps
-#   scope                = local.container_registry_resource_id
-#   role_definition_name = "AcrPull"
-#   principal_id         = azurerm_user_assigned_identity.this[each.key].principal_id
-# }
+  tags = local.tags
+}
+
+module "cae" {
+  source  = "Azure/avm-res-app-managedenvironment/azurerm"
+  version = "0.2.1"
+
+  name                = local.container_app_environment_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  log_analytics_workspace_customer_id        = azurerm_log_analytics_workspace.law.id
+  log_analytics_workspace_primary_shared_key = azurerm_log_analytics_workspace.law.primary_shared_key
+
+  tags = local.tags
+}
+
+# Container App
+resource "azurerm_container_app" "this" {
+  for_each                     = local.container_apps
+  name                         = "ca-${each.key}-${local.application_name}-${local.environment}"
+  resource_group_name          = azurerm_resource_group.this.name
+  container_app_environment_id = module.cae.id
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "ca-${each.key}-${local.application_name}-${local.environment}"
+      image  = each.value.image
+      cpu    = 0.25
+      memory = "0.5Gi"
+    }
+  }
+
+  ingress {
+    target_port      = each.value.port
+    external_enabled = each.value.external_enabled
+
+    traffic_weight {
+      percentage      = 100
+      latest_revision = true
+    }
+  }
+
+  registry {
+    server   = "acrmanacr.azurecr.io"
+    identity = azurerm_user_assigned_identity.this[each.key].id
+  }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.this[each.key].id
+    ]
+  }
+}
+
+# Container App - User Assigned Identity
+resource "azurerm_user_assigned_identity" "this" {
+  for_each            = local.container_apps
+  name                = "uai-${each.key}-${local.application_name}-${local.environment}"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+# Container App - ACR Pull
+resource "azurerm_role_assignment" "acrpull" {
+  for_each             = local.container_apps
+  scope                = local.container_registry_resource_id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.this[each.key].principal_id
+}
 
 
 
