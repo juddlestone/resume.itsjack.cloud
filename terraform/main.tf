80426830e2645c5e3650a9cf217d1b84f41a8f63
juddlestone <jack@itsjack.cloud>
2025-03-01 22:21:00 +0000

    Amend code comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,16 @@
-data "azurerm_client_config" "current" {
-}
-
+# Azure Naming Module
+# Ensures all resources names are aligned with recommended naming conventions
 module "naming" {
   source = "Azure/naming/azurerm"
   suffix = [local.application_name, local.environment]
 }
 
+# Time Static
+# This is used to determine the start and end dates for the budget
 resource "time_static" "this" {}
 
 # Resource Group
+# Where all resources will be created
 resource "azurerm_resource_group" "this" {
   name     = local.resource_group_name
   location = local.location
@@ -17,6 +19,7 @@ resource "azurerm_resource_group" "this" {
 }
 
 # Budget
+# Budget on the resource group to ensure I keep an eye on costs
 resource "azurerm_consumption_budget_resource_group" "this" {
   name              = local.budget_name
   resource_group_id = azurerm_resource_group.this.id
@@ -36,6 +39,7 @@ resource "azurerm_consumption_budget_resource_group" "this" {
 }
 
 # Log Analytics Workspace
+# Used to collect logs from the container app
 resource "azurerm_log_analytics_workspace" "law" {
   name                = local.log_analytics_workspace_name
   location            = azurerm_resource_group.this.location
@@ -47,18 +51,18 @@ resource "azurerm_log_analytics_workspace" "law" {
 }
 
 # Container App Environment
+# Use the AVM module to create a container app environment
 module "cae" {
   source  = "Azure/avm-res-app-managedenvironment/azurerm"
   version = "0.2.1"
 
-  name                = local.container_app_environment_name
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  name                    = local.container_app_environment_name
+  resource_group_name     = azurerm_resource_group.this.name
+  location                = azurerm_resource_group.this.location
+  zone_redundancy_enabled = false
 
   log_analytics_workspace_customer_id        = azurerm_log_analytics_workspace.law.workspace_id
   log_analytics_workspace_primary_shared_key = azurerm_log_analytics_workspace.law.primary_shared_key
-
-  zone_redundancy_enabled = false
 
   storages = {
     "visitor-data" = {
@@ -72,8 +76,8 @@ module "cae" {
   tags = local.tags
 }
 
-
 # Container App
+# Use the AVM module to create a container app
 module "container_app" {
   source  = "Azure/avm-res-app-containerapp/azurerm"
   version = "0.3.0"
@@ -95,12 +99,16 @@ module "container_app" {
         memory = "0.5Gi"
         env = [
           {
+            # This is the endpoint to the storage account
+            # It is used to access the certification pictures, keeping docker image small
             name  = "BLOB_ENDPOINT"
             value = "${azurerm_storage_account.this.primary_blob_endpoint}certifications/"
           }
         ]
         volume_mounts = [
           {
+            # This mounts an Azure File Share to the container
+            # It is used to store visitor data
             name = "visitor-data"
             path = "/visitor-data"
           }
@@ -108,10 +116,9 @@ module "container_app" {
       }
     ],
 
-
-
     volumes = [
       {
+        # This is the Azure File Share that associated with the container app environment
         name         = "visitor-data"
         storage_name = "visitor-data"
         storage_type = "AzureFile"
@@ -120,10 +127,14 @@ module "container_app" {
   }
 
   ingress = {
+    # External allowed
+    # Flask app is running on port 5000
     allow_insecure_connections = false
     external_enabled           = true
     target_port                = 5000
     transport                  = "http"
+
+    # Even though it is a single revision, this is needed.
     traffic_weight = [{
       latest_revision = true
       percentage      = 100
@@ -131,6 +142,7 @@ module "container_app" {
   }
 
   custom_domains = {
+    # Custom domain for the container app
     domain = {
       name                     = var.custom_domain
       certificate_binding_type = "SniEnabled"
@@ -138,11 +150,13 @@ module "container_app" {
   }
 
   managed_identities = {
+    # This is the user assigned identity that is used to access the ACR
     user_assigned_resource_ids = [azurerm_user_assigned_identity.this.id]
   }
 
   registries = [
     {
+      # This is the ACR that contains the container image
       identity = azurerm_user_assigned_identity.this.id
       server   = local.container_registry_url
     }
@@ -152,14 +166,15 @@ module "container_app" {
 }
 
 # Container App - User Assigned Identity
+# This is used to access the ACR
 resource "azurerm_user_assigned_identity" "this" {
   name                = local.user_assigned_identity_name
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
 
-
 # Container App - ACR Pull
+# This is used to allow the container app to pull images from the ACR
 resource "azurerm_role_assignment" "acrpull" {
   scope                = local.container_registry_resource_id
   role_definition_name = "AcrPull"
@@ -167,6 +182,7 @@ resource "azurerm_role_assignment" "acrpull" {
 }
 
 # Storage Account
+# Used to store the certification pictures and visitor data
 resource "azurerm_storage_account" "this" {
   name                     = module.naming.storage_account.name_unique
   resource_group_name      = azurerm_resource_group.this.name
@@ -176,6 +192,7 @@ resource "azurerm_storage_account" "this" {
 }
 
 # Storage Container
+# Used to store the certification pictures
 resource "azurerm_storage_container" "certifications" {
   name                  = "certifications"
   storage_account_id    = azurerm_storage_account.this.id
@@ -183,6 +200,7 @@ resource "azurerm_storage_container" "certifications" {
 }
 
 # Upload files to Storage Container
+# Upload the certification pictures to the storage container
 resource "azurerm_storage_blob" "certifications" {
   for_each               = fileset("${path.root}/blobs/certifications", "*")
   name                   = each.value
@@ -194,6 +212,7 @@ resource "azurerm_storage_blob" "certifications" {
 }
 
 # Storage File
+# Used to store visitor data
 resource "azurerm_storage_share" "this" {
   name               = "visitor-data"
   storage_account_id = azurerm_storage_account.this.id
